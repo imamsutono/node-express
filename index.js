372c51e6ca1b3d14f1dc5f7e75dc76844ef5b05e
Stephen Daniel <stephendaniel60@gmail.com>
2020-12-22 14:15:49 +0700

    Add userse http method

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/about', function(req, res) {
+  res.send('About');
+});
+
+// Users route
+app.get('/users', function(req, res) {
+  res.send('Get users');
+});
+app.post('/users', function(req, res) {
+  res.send('Post users')
+});
+app.put('/users/:id', function(req, res) {
+  const id = req.params;
+  res.send(id);
+});
+app.delete('/users/:userId', function(req, res) {
+  const id = req.params.userId;
+  res.send(id);
+});
+
 app.listen(3000, function() {
   console.log('server is okay');
 });
