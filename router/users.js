11126191dbb67ccc5d423d4c8e2308c339e6a424
Stephen Daniel <stephendaniel60@gmail.com>
2020-12-22 16:11:34 +0700

    Push data to users

--- router/users.js
+++ router/users.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+let users = [
+  {id: 1, name: 'Imam', email: 'imam@imno.com'},
+  {id: 2, name: 'Sutono', email: 'sutono@imno.com'}
+];
+
 router.route('/users')
   .get(function(req, res) {
-    res.send('Get users');
+    res.json(users);
   })
   .post(function(req, res) {
-    res.send('Post users')
+    users.push(req.body);
+    res.send(users);
   });
 router.put('/users/:id', function(req, res) {
   const id = req.params;
